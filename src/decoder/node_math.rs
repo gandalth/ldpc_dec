fb21273110ce7868384026625f11d229e83fd564
th <th@tp2>
2026-03-14 07:50:15 +0100

    Memory allocation: avoid .collect().

--- src/decoder/node_math.rs
+++ src/decoder/node_math.rs
@@ -4,33 +4,33 @@ pub fn normalized_mult_exc_one(f0: &[f32]) -> Vec<f32> {
     if n == 0 {
 	return Vec::new();
     }
-    
+
     let mut prefix_f0 = vec![1.0; n];
     let mut suffix_f0 = vec![1.0; n];
     let mut prefix_f1 = vec![1.0; n];
     let mut suffix_f1 = vec![1.0; n];
-				    
+    let mut result    = vec![0.0; n];
+
     for i in 1..n {
         prefix_f0[i] = prefix_f0[i - 1] * f0[i - 1];
 	prefix_f1[i] = prefix_f1[i - 1] * (1.0 - f0[i - 1]);
     }
-    
+
     for i in (0..n - 1).rev() {
         suffix_f0[i] = suffix_f0[i + 1] * f0[i + 1];
 	suffix_f1[i] = suffix_f1[i + 1] * (1.0 - f0[i + 1]);
     }
-    
-    (0..n)
-        .map(|i| {
-	    let num = prefix_f0[i] * suffix_f0[i];
-	    let den = num + prefix_f1[i] * suffix_f1[i];
-	    if den == 0.0 {
-		0.5
+
+    for i in 0..n {
+	let num = prefix_f0[i] * suffix_f0[i];
+	let den = num + prefix_f1[i] * suffix_f1[i];
+	if den == 0.0 {
+		result[i] = 0.5
 	    } else {
-		num / den
-	    }
-	})
-        .collect()
+		result[i] = num / den
+	}
+    }
+    result
 }
 
 pub fn gallager_prod_exc_one(f0: &[f32]) -> Vec<f32> {
