37db1cb3ff5655bde35141f0cdb693d14d34c28a
th <th@tp2>
2026-09-01 10:52:27 +0200

    Make simulation type accessible from command line.

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,24 @@ mod sim_jd;
 mod sim_css;
 
 fn main() {
-    //sim_classic::run();
-    //sim_jd::run();
-    sim_css::run();
+    let sim = std::env::args()
+        .nth(1)
+        .unwrap_or_else(|| {
+            eprintln!("Usage: cargo run -- [classic|jd|css]");
+            std::process::exit(1);
+        });
+
+    match sim.as_str() {
+        "classic" => sim_classic::run(),
+        "jd"      => sim_jd::run(),
+        "css"     => sim_css::run(),
+
+        _ => {
+            eprintln!("Unknown simulation: {}", sim);
+            eprintln!("Usage: cargo run -- [classic|jd|css]");
+            std::process::exit(1);
+        }
+    }
 }
 
 
